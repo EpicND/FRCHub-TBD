f2c278c693701dc03eec6f7dc22975a8507217a6
ccans <55026208+ccans@users.noreply.github.com>
2020-03-14 14:31:33 -0500

    eBic

--- main/js/jStyles.js
+++ main/js/jStyles.js
@@ -21,11 +21,46 @@ $(document).ready(function() {
 
 function checkFilled() {
    if($('.input-3').val().length > 0) {
-     document.getElementById("adj-button").value="Submit";
+     document.getElementById("adj-button").value="My team is " + teamNameArray[teamNumberArray.indexOf(parseInt($('.input-3').val()))];
    } else {
      document.getElementById("adj-button").value="Skip";
    }
 }
+
+var teamNameArray =[];
+var teamNumberArray =[];
+// ONLY SUPPORTS 9k FRC teams
+
+for(p=0; p < 30; p++) {
+
+
+var teamNameRequest = new XMLHttpRequest();
+teamNameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/" + p + "/simple", true);
+teamNameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+teamNameRequest.send();
+
+teamNameRequest.onload = function() {
+  if (this.readyState == 4 && this.status == 200){
+      var teamNameRequestObj = JSON.parse(this.responseText);
+      var i;
+      if(teamNameRequestObj.length < 1) {
+      } else {
+      for (i = 0; i < teamNameRequestObj.length; i++) {
+          teamNameArray.push(teamNameRequestObj[i].nickname);
+          teamNumberArray.push(teamNameRequestObj[i].team_number);
+        }
+      }
+  }
+  }
+
+
+
+}
+
+
+
+
+
 
 $(document).ready(function() {
   var inputs = $("input");
