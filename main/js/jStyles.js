8ab1a60bb34acedac94d953737f3f9ffb1b08924
ccans <55026208+ccans@users.noreply.github.com>
2020-03-15 12:34:17 -0500

    More Fade

--- main/js/jStyles.js
+++ main/js/jStyles.js
@@ -13,7 +13,12 @@ $(document).ready(function() {
       setTimeout(function () {
       $(".section-1").animate({opacity:1}, 1000);
       $(".subbtn-container").animate({opacity:1}, 3000);
-
+            setTimeout(function () {
+                $('.auth-login').animate({opacity:1}, 1000);
+                setTimeout(function () {
+                $('.test-email').animate({opacity:1}, 1000)
+                    }, 100);
+                }, 500);
       }, 500);
   }, 1000);
 
