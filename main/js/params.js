c682e70215f268396593f5cfd8953b39981f1bac
DhairyaG23 <dhairyag23@gmail.com>
2020-03-24 00:21:16 -0500

    more redundancy

--- main/js/params.js
+++ main/js/params.js
@@ -3,5 +3,5 @@ function directLink(){
 if(document.getElementById('in-3').value !== "" && isTeam == true){
   window.location.href = 'main/profile.html'+'?teamID=' + document.getElementById("in-3").value;
 }
-
+document.getElementById('in-3').value = "";
 }
